3239e313e40e4d2f943d866462c39a2234b6a746
maria <leelya85@gmail.com>
2017-09-07 22:53:43 +0530

    Implement Meme 1.0 App

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,46 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var secondView: UIView!
     
     @IBOutlet weak var shareButton: UIBarButtonItem!
     
-    @IBOutlet weak var imagePicked: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     
     @IBOutlet weak var chooseImageLabel: UILabel!
     
-    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     @IBOutlet weak var albumButton: UIBarButtonItem!
     
-   // let topTextField = TextField()
+    @IBOutlet weak var bottomTextField: UITextField!
     
-   // let bottomTextField = TextField()
-
+    @IBOutlet weak var topTextField: UITextField!
+    
+    var memedImage: UIImage?
+    
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+    }
+    
+    /*
+     Code Reference : https://stackoverflow.com/questions/26070242/move-view-with-keyboard-using-swift
+     */
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            print(self.topTextField.isFirstResponder)
+            if self.bottomTextField.isFirstResponder && self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    // Presents an image picker if camera is available
     @IBAction func openCamera(_ sender: Any) {
         
-        print("in camera")
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
@@ -57,6 +81,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
+    // Presents an image picker with Photo Album source type.
     @IBAction func openAlbum(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let imagePicker = UIImagePickerController()
@@ -67,20 +92,73 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        picker.dismiss(animated: true, completion: nil)
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        imagePicked.image = image
+    // Clears the image and text, unhides label and dismisses keyboard
+    @IBAction func cancel(_ sender: Any) {
+        dismissKeyboard()
+        imageView.image = nil
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        chooseImageLabel.isHidden = false
     }
     
+    func dismissKeyboard() {
+        topTextField.resignFirstResponder()
+        bottomTextField.resignFirstResponder()
+    }
+    
+    // Creates Meme image and launches Apple's stock Activity view
     @IBAction func share(_ sender: Any) {
-        let imageToShare = [ imagePicked.image! ]
+        
+        dismissKeyboard()
+        generateMemedImage()
+        
+        guard let image = memedImage else {
+            return
+        }
+        
+        let imageToShare = [ image ]
         let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
         activityViewController.popoverPresentationController?.sourceView = self.view
         self.present(activityViewController, animated: true, completion: nil)
     }
-
-   
+    
+    // Generats the image with top and bottom text by rendering the view.
+    func generateMemedImage() {
+        UIGraphicsBeginImageContext(self.secondView.bounds.size)
+        secondView.drawHierarchy(in: self.secondView.bounds, afterScreenUpdates: true)
+        memedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
 }
 
+// UIImagePickerControllerDelegate Methods
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        chooseImageLabel.isHidden = true
+    }
+    
+}
+
+// UITextFieldDelegate Methods
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.text = nil
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == topTextField {
+            bottomTextField.becomeFirstResponder()
+        } else {
+            bottomTextField.resignFirstResponder()
+        }
+        return true
+    }
+    
+}
